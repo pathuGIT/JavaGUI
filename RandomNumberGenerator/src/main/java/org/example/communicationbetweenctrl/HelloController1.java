2fb632019d1c5e1d4737d955d1eec6d6a38b8682
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-01-21 20:14:51 +0530

    updated this app

--- RandomNumberGenerator/src/main/java/org/example/communicationbetweenctrl/HelloController1.java
+++ RandomNumberGenerator/src/main/java/org/example/communicationbetweenctrl/HelloController1.java
@@ -2,16 +2,47 @@ package org.example.communicationbetweenctrl;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.util.Random;
 
 public class HelloController1{
     @FXML
     Label lbl1;
+    @FXML
+    private TextField input1;
+    @FXML
+    private TextField input2;
+
+
     public void genNum(){
-        //System.out.println("Hi");
-        Random rnd = new Random();
-        int value = rnd.nextInt(50);
-        lbl1.setText(Integer.toString(value));
+        try{
+            String min = input1.getText();
+            String max = input2.getText();
+            int Min = Integer.parseInt(min);
+            int Max = Integer.parseInt(max);
+
+            if (min.isEmpty() || max.isEmpty()) {
+
+                lbl1.setText("Please enter values for both Min and Max.");
+                System.out.println("empty");
+
+            }else{
+
+                Random rnd = new Random();
+                int value = rnd.nextInt(Max - Min +1) + Min;
+                lbl1.setText(Integer.toString(value));
+            }
+
+        }catch(NumberFormatException e){
+            System.out.println("Wrong");
+            lbl1.setText("Min & Max ?");
+        }
+
+
+
+
+
+
     }
 }
