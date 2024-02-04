df3cf54717a6fd2ca63e466698351299469ce6d5
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2024-02-04 09:39:29 +0530

    new app

--- LoginApp2/src/main/java/org/example/loginapp2/HelloController.java
+++ LoginApp2/src/main/java/org/example/loginapp2/HelloController.java
@@ -2,15 +2,21 @@ package org.example.loginapp2;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class HelloController {
+    private Stage stage;
     @FXML
     private Button cancelButton;
     @FXML
@@ -23,7 +29,6 @@ public class HelloController {
 
     public void loginButtton(ActionEvent e){
         if(userName.getText().isBlank() == false && userPwd.getText().isBlank() == false){
-//          errorId.setText("You are trying to login");
             validLogin();
         }else{
             errorId.setText("Something wrong");
@@ -34,37 +39,19 @@ public class HelloController {
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
-
-//    public void validLogin(){
-//        dbConnect connect = new dbConnect();
-//        Connection connDb = connect.getConnection();
-//
-//        String validLogin = "select count(1) from users where username = '"+userName+"' and password = '"+userPwd+"'";
-//
-//        try{
-//            Statement statement = connDb.createStatement();
-//            ResultSet qeryResult = statement.executeQuery(validLogin);
-////            while (qeryResult.next()){
-////                if(qeryResult.getInt(1)==1){
-////                    System.out.println("Welcome");
-////                    errorId.setText("Welcome");
-////                }else{
-////                    errorId.setText("Invalid Login");
-////                    System.out.println("Wrong");
-////                }
-////            }
-//            System.out.println(qeryResult.getRow());
-//        }catch(Exception e){
-//
-//        }
-
-//    }
+    public void switchToSce1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view1.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void validLogin() {
         dbConnect connect = new dbConnect();
-        Connection connDb = connect.getConnection();
+        Connection connDb = connect.studentConnection();
 
-        String selectQuery = "SELECT * FROM users WHERE username = ? AND password = ?";
+        String selectQuery = "SELECT * FROM student WHERE Std_id = ? AND Password = ?";
 
         try (PreparedStatement preparedStatement = connDb.prepareStatement(selectQuery)) {
             preparedStatement.setString(1, userName.getText());
@@ -73,16 +60,22 @@ public class HelloController {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
-                // Valid login
-                System.out.println("Welcome");
                 errorId.setText("Welcome");
-            } else {
-                // Invalid login
-                errorId.setText("Invalid Login");
-                System.out.println("Wrong");
+                switchToSce1();
+            } else if (userName.getText().equals("DEAN") && userPwd.getText().equals("DE123")){
+                errorId.setText("Welcome Dean");
+            } else if (userName.getText().equals("ADMIN")  && userPwd.getText().equals("Admin123")) {
+                errorId.setText("Welcome ADMIN");
+            } else if (userName.getText().equals("LECTURE") && userPwd.getText().equals("Lec123")) {
+                errorId.setText("Welcome Lecture");
+            }else{
+                errorId.setText("You are trying to login.");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+
+
 }
--- LoginApp2/src/main/java/org/example/loginapp2/dbConnect.java
+++ LoginApp2/src/main/java/org/example/loginapp2/dbConnect.java
@@ -6,9 +6,25 @@ import java.sql.SQLException;
 
 public class dbConnect {
     public Connection datalink;
-    public Connection getConnection(){
-        String dbName = "class";
-        String dbUser = "root";
+    public Connection studentConnection(){
+        String dbName = "tecmis_teclms";
+        String dbUser = "STUDENT";
+        String dbPwd = "1234";
+        String url = "jdbc:mysql://localhost:3306/" + dbName;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            datalink = DriverManager.getConnection(url, dbUser, dbPwd);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return datalink;
+    }
+
+    public Connection deanConnection(){
+        String dbName = "tecmis_teclms";
+        String dbUser = "DEAN";
         String dbPwd = "1234";
         String url = "jdbc:mysql://localhost:3306/" + dbName;
 
